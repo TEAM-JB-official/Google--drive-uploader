b9582e123c971efe9cd7dce8fc90d822c04fcc03
TEAM-JB-official <teamjb80@gmail.com>
2026-06-13 23:14:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", "25331263"))
+API_HASH = os.getenv("API_HASH", "cab85305bf85125a2ac053210bcd1030")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7977209272:AAEX0GrXV0hjWPJx6E_HLq-uOjAlqd7mul4")
 MONGO_URL = os.getenv("MONGO_URL", "")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "")
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
-DOMAIN = os.getenv("DOMAIN", "")
-REDIRECT_URI = os.getenv("REDIRECT_URI", "")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1003909289740"))
+DOMAIN = os.getenv("DOMAIN", "https://integral-kial-jsssbeniwa-7e4dcaa1.koyeb.app")
+REDIRECT_URI = os.getenv("REDIRECT_URI", "https://integral-kial-jsssbeniwa-7e4dcaa1.koyeb.app/auth/callback")
 ADMIN_IDS = [int(x.strip()) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
 
 # Plan limits (uploads per day, parallel uploads, accounts)
